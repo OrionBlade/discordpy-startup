eddabd46c871a60a3e1a7cfdb4a45a08c0df41f7
OrionBlade <69398015+OrionBlade@users.noreply.github.com>
2020-08-09 03:21:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,7 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
+#test
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
