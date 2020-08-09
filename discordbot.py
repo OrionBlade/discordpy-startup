65af3490bcc24e1598b823dfd0e6aafb8d76f823
OrionBlade <69398015+OrionBlade@users.noreply.github.com>
2020-08-09 23:21:21 +0900

    006

--- discordbot.py
+++ discordbot.py
@@ -17,15 +17,5 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('ponpon')
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+
 bot.run(token)
